30774df025aa39ac5993c1d2ae41cb8fae755f1e
moraleszez <moraleszez@gmail.com>
2021-01-29 00:00:42 -0600

    Added event timer

---
+++ Assets/0_Game/Scripts/VarsFramework/EventTimer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class EventTimer : MonoBehaviour
+{
+    public float time;
+    public UnityEvent onTimerTock;
+
+    public void StartTimer()
+    {
+        StartCoroutine(Tick());
+    }
+
+    IEnumerator Tick()
+    {
+        yield return new WaitForSeconds(time);
+        onTimerTock.Invoke();
+    }
+}
